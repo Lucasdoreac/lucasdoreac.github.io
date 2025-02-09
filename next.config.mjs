63fbabe6b1d8d8529ced2877a7e84fd7f1e4a6ba
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-09 20:52:59 -0300

    Update Next.js config for correct output path

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
-  distDir: 'dist',
+  basePath: '',
   trailingSlash: true,
 }
-
-export default nextConfig
