e3762cd3460ed154019bf3977fd072c2911ddfc8
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-09 20:57:02 -0300

    Add simple landing page to verify deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Projects from '@/components/Projects'
-
 export default function Home() {
   return (
-    <main className="bg-white dark:bg-primary min-h-screen">
-      <Header />
-      <Hero />
-      <Projects />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold">Lucas Dórea</h1>
+        <p>Portfolio Website - Coming Soon</p>
+      </div>
     </main>
   )
 }
